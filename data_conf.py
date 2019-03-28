870ca250afc7c8d333fc97994af14b7a92fc5b92
Rohan Dudam <rohan.dudam@sciencelogic.com>
2019-03-28 13:39:48 +0530

    modified board name. Board name contains board number, name, and date in expected format

--- data_conf.py
+++ data_conf.py
@@ -1,5 +1,7 @@
 board_name=["Test Board 2","Test Board 2"]
 list_names=["TODO","DOING","DONE"]
+board_number= 236                           # Provide Board Number here
+startdate = datetime.datetime(YYYY,MM,DD)    # Update date here YYYY,MM,DD
 sample_board=""
 sample_cards=["",""]
 member_ids=["",""]
--- test_trello_util.py
+++ test_trello_util.py
@@ -6,13 +6,17 @@ from conf import key,token
 import time
 from data_conf import board_name,list_names,sample_board,sample_cards,member_ids,member_username
 import pickle
+import datetime
+import pandas as pd
 
 def test_trello_util(key,token):
     # Creating an object of Trello Util
     test_obj = Trello_Util(key,token)
     # Get the board names from conf
     board_names=board_name
-    new_board_name = board_names[0]
+    date = pd.to_datetime(startdate) + pd.DateOffset(days=7)
+    new_board_name = str(board_number) + "." + str(board_names[0]) + "("+ str(date.strftime("%d-%b-%Y")) + ")"
+    print (new_board_name)
     
     # Create a new board
     result_flag = test_obj.add_board(new_board_name)
